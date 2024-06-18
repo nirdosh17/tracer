e331484fd3d2d40e26114db676871ff6fb8ae399
Nirdosh Gautam <nrdshgtm@gmail.com>
2024-06-18 13:53:16 +0545

    add retry option display location of private IPs as 'private range'

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Tracer
 A packet tracer in Go similar to Traceroute.
 
-Makes UDP call to target host increasing the TTL(hops) of IP packet and recording the ICMP response for each hop until it finally reaches the destination or max TTL is reached.
+Makes UDP call to target host increasing the TTL(hops) of IP packet and recording the ICMP response for each hop(router address) until it finally reaches the destination or max TTL is reached.
 
 
 ### Test
--- locate.go
+++ locate.go
@@ -7,35 +7,39 @@ import (
 	"net/http"
 )
 
-// geoResponse represents the response from the geolocation API
+// check available fields here: https://ip-api.com/#8.8.8.8
 type geoResponse struct {
-	IP      string `json:"query"`
-	Country string `json:"country"`
-	Region  string `json:"regionName"`
-	City    string `json:"city"`
-	Org     string `json:"org"`
+	Country      string `json:"country"`
+	Isp          string `json:"isp"`
+	QueryStatus  string `json:"status"`
+	QueryMessage string `json:"message"`
 }
 
-func locateIP(ip string) geoResponse {
-	defaultResp := geoResponse{}
+func locateIP(ip string) string {
 	url := fmt.Sprintf("http://ip-api.com/json/%s", ip)
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println("[locateIP] failed to fetch location info for IP", ip, err)
-		return defaultResp
+		return "N/A"
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("[locateIP] failed to read resp body:", err)
-		return defaultResp
+		return "N/A"
 	}
 
 	var geo geoResponse
 	err = json.Unmarshal(body, &geo)
 	if err != nil {
 		fmt.Println("[locateIP] error unmarshaling response:", err)
+		return "N/A"
 	}
-	return geo
+
+	if geo.QueryStatus == "fail" {
+		return geo.QueryMessage
+	}
+
+	return fmt.Sprintf("%v (%v)", geo.Isp, geo.Country)
 }
