43762909d842e746de7e2853db6a680cd8ecfd37
Nirdosh Gautam <nrdshgtm@gmail.com>
2024-06-20 15:48:53 +0545

    get public ip using STUN request

--- cmd/main.go
+++ cmd/main.go
@@ -4,49 +4,94 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"os"
 	"sync"
 
 	"github.com/nirdosh17/tracer"
 )
 
+var colorReset = "\033[0m"
+var colorGreen = "\033[32m"
 var usage = `
 Usage:
 
-	It requires privileged access to work with raw ICMP packets.
+	1. Tracing route requires privileged access to work with raw ICMP packets.
 	Make sure to run the command as administrator.
 
-	sudo trace [-hops] [-timeout] host
+		` + colorGreen + `sudo gotrace route [-hops] [-timeout] host` + colorReset + `
+
+	2. Get your public ip
+		` + colorGreen + `gotrace myip` + colorReset + `
 
 Examples:
 
 	# trace with default settings, max hops: ` + fmt.Sprintf("%v", tracer.DEFAULT_HOPS) + `, timeout(seconds): ` + fmt.Sprintf("%v", tracer.DEFAULT_TIMEOUT_SECONDS) + `, retries: ` + fmt.Sprintf("%v", tracer.DEFAULT_MAX_RETRIES) + `
-	trace google.com
+	` + colorGreen + `gotrace google.com` + colorReset + `
 
 	# trace 'n' number of hops
-	trace -hops 10 example.com
+	` + colorGreen + `gotrace -hops 10 example.com` + colorReset + `
 
 	# if you are receiving blank response
 	# try increasing the ICMP response timeout(-t) and retries(-r)
-	trace -t 10 -r 5 example.com
+	` + colorGreen + `gotrace -t 10 -r 5 example.com` + colorReset + `
+
+	# get your public ip
+	` + colorGreen + `gotrace myip` + colorReset + `
 `
 
 func main() {
-	hops := flag.Int("hops", tracer.DEFAULT_HOPS, "")
-	timeout := flag.Int("t", tracer.DEFAULT_TIMEOUT_SECONDS, "")
-	retries := flag.Int("r", tracer.DEFAULT_MAX_RETRIES, "")
+	routeCmd := flag.NewFlagSet("route", flag.ExitOnError)
+	hops := routeCmd.Int("hops", tracer.DEFAULT_HOPS, "")
+	timeout := routeCmd.Int("t", tracer.DEFAULT_TIMEOUT_SECONDS, "")
+	retries := routeCmd.Int("r", tracer.DEFAULT_MAX_RETRIES, "")
+
 	flag.Usage = func() {
 		fmt.Println(usage)
 	}
 
-	flag.Parse()
-
-	if flag.NArg() == 0 {
+	if len(os.Args) == 1 {
 		flag.Usage()
 		return
 	}
 
-	host := flag.Arg(0)
-	addr, _ := net.ResolveIPAddr("ip", host)
+	// first arg is always binary name e.g. /tmp/go-build3122800919/b001/exe/main
+	switch os.Args[1] {
+	case "route":
+		routeCmd.Parse(os.Args[2:])
+		if routeCmd.NArg() == 0 {
+			flag.Usage()
+			return
+		}
+
+		host := routeCmd.Arg(0)
+		traceRoute(host, hops, retries, timeout)
+
+	case "myip":
+		pubIP, err := tracer.PublicIP()
+		if err != nil {
+			fmt.Println("failed to fetch your public ip:", err)
+			os.Exit(1)
+		}
+		fmt.Println("Your Public IP:")
+		fmt.Println(pubIP)
+
+	case "-h", "-help":
+		flag.Usage()
+
+	default:
+		fmt.Printf("unknown command '%v', run 'trace -help' for command usage\n", os.Args[1])
+		os.Exit(1)
+	}
+
+}
+
+func traceRoute(host string, hops, retries, timeout *int) {
+	addr, err := net.ResolveIPAddr("ip", host)
+	if err != nil {
+		fmt.Printf("failed to resolve host '%v'\n", host)
+		os.Exit(1)
+	}
+
 	fmt.Printf("tracing %v (%v), %v hops max, max retries: %v\n", host, addr.String(), *hops, *retries)
 
 	// consume live hops from channel
@@ -68,11 +113,12 @@ func main() {
 
 	config := tracer.NewConfig().WithHops(*hops).WithTimeout(*timeout)
 	t := tracer.NewTracer(config)
-	_, err := t.Run(host, c)
+	_, err = t.Run(host, c)
 	wg.Wait()
 
 	if err != nil {
-		fmt.Println("Error from tracer: ", err)
+		fmt.Printf("failed to trace route for %v: %v\n", host, err)
+		os.Exit(1)
 	}
 }
 
